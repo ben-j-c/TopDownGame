0e33e1371a892a581fc6a95422925278bd49a7af
ben-j-c <benjamin.j.correia@gmail.com>
2017-03-24 00:01:16 -0400

    Cleaned some code
    Moved KeyList to its own class.

---
+++ TopDownShooter/src/game/KeyList.java
@@ -0,0 +1,13 @@
+package game;
+
+final class KeyList
+{
+	public boolean UP = false;
+	public boolean DOWN = false;
+	public boolean LEFT = false;
+	public boolean RIGHT = false;
+	public boolean MOUSE_LEFT = false;
+	public boolean MOUSE_RIGHT = false;
+	public double  MOUSE_X = 0;
+	public double  MOUSE_Y = 0;
+}
--- TopDownShooter/src/game/Shoot.java
+++ TopDownShooter/src/game/Shoot.java
@@ -21,24 +21,6 @@ import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
 
-
-
-/**
- * 
- * @author Ben
- *
- */
-final class KeyList
-{
-	public boolean UP = false;
-	public boolean DOWN = false;
-	public boolean LEFT = false;
-	public boolean RIGHT = false;
-	public boolean MOUSE_LEFT = false;
-	public boolean MOUSE_RIGHT = false;
-	public double  MOUSE_X = 0;
-	public double  MOUSE_Y = 0;
-}
 /**
  * 
  * @author Ben
